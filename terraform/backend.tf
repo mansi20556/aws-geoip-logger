57bf64a2c5a5ae96c1bd72f23e805017b722784f
Mansi Jain <jainmansi668@gmail.com>
2025-05-14 13:15:12 +0530

    initial deployment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "mansi-geoip-tf-state"
     key            = "geoip/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
 }
