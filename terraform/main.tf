c6e35ca91e392eddf10317fbff87837075773222
Mansi Jain <jainmansi668@gmail.com>
2025-05-14 13:56:47 +0530

    Working GeoIP Logger with CloudWatch and DynamoDB permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,22 +2,23 @@ provider "aws" {
   region = var.region
 }
 
-# Upload Lambda source code
+# Zip the Lambda source
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "../lambda"
   output_path = "../lambda/geoip_logger.zip"
 }
 
+# S3 bucket (for logs if you want to extend later)
 resource "aws_s3_bucket" "logs" {
-  bucket = var.s3_bucket_logs
+  bucket        = var.s3_bucket_logs
   force_destroy = true
 }
 
+# DynamoDB Table to store logs
 resource "aws_dynamodb_table" "geoip_logs" {
   name         = var.ddb_table_name
   billing_mode = "PAY_PER_REQUEST"
-
   hash_key     = "ip"
 
   attribute {
@@ -26,6 +27,7 @@ resource "aws_dynamodb_table" "geoip_logs" {
   }
 }
 
+# Lambda Execution Role
 resource "aws_iam_role" "lambda_exec" {
   name = "geoip_lambda_exec_role"
 
@@ -41,12 +43,35 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
-resource "aws_iam_policy_attachment" "lambda_policy" {
-  name       = "geoip-attach"
-  roles      = [aws_iam_role.lambda_exec.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# Inline policy for Lambda to access DynamoDB and CloudWatch Logs
+resource "aws_iam_role_policy" "lambda_inline_policy" {
+  name = "geoip-inline-policy"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem"
+        ]
+        Resource = aws_dynamodb_table.geoip_logs.arn
+      }
+    ]
+  })
 }
 
+# Lambda Function
 resource "aws_lambda_function" "geoip_logger" {
   function_name = var.lambda_function_name
   handler       = "geoip_logger.lambda_handler"
@@ -60,34 +85,40 @@ resource "aws_lambda_function" "geoip_logger" {
     }
   }
 
-  depends_on = [aws_iam_policy_attachment.lambda_policy]
+  depends_on = [
+    aws_iam_role_policy.lambda_inline_policy
+  ]
 }
 
+# HTTP API Gateway (v2)
 resource "aws_apigatewayv2_api" "api" {
   name          = "geoip-logger-api"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.geoip_logger.invoke_arn
-  integration_method = "POST"
-  payload_format_version = "2.0"
+  api_id                  = aws_apigatewayv2_api.api.id
+  integration_type        = "AWS_PROXY"
+  integration_uri         = aws_lambda_function.geoip_logger.invoke_arn
+  integration_method      = "POST"
+  payload_format_version  = "2.0"
 }
 
+# Route: GET /log
 resource "aws_apigatewayv2_route" "route" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "GET /log"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
+# $default stage with auto-deploy enabled
 resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
 }
 
+# Allow API Gateway to invoke Lambda
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
